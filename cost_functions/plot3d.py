3835706adfefbc2bb9a0ed92b0bc7023e5e616ff
ddfabbro <fabda01@gmail.com>
2017-12-23 11:55:54 -0200

    Add example 3d plot figure

---
+++ cost_functions/plot3d.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 23 01:03:24 2017
+
+@author: davi
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from benchmark import ContinuousFunction
+
+function = ContinuousFunction()
+
+fig = plt.figure()
+ax = Axes3D(fig, azim = -120, elev = 40)
+
+x1 = np.linspace(-5, 5, 100)
+x2 = np.linspace(-5, 5, 100)
+X = np.array(np.meshgrid(x1, x2))
+Z = function.venkataraman(X)
+
+ax.plot_surface(X[0], X[1], Z, cmap='jet', linewidth=.2, edgecolor='black')
+ax.set_xlim([-5, 5])                                                       
+ax.set_ylim([-5, 5])
+
+plt.savefig('3dplot.jpg')
