bbe6b5ff2e9e921f5774160973da6450684b6208
ddfabbro <fabda01@gmail.com>
2018-02-27 18:50:09 -0300

    Improve kriging image

--- examples/kriging_example.py
+++ examples/kriging_example.py
@@ -10,28 +10,22 @@ from pyooopt.cost_functions import ContinuousFunction
 class MyKriging(kriging):
     def __init__(self,*args,**kwargs):
         kriging.__init__(self,*args,**kwargs)
-    def kdata(self):
+    def kplot(self,i):
         plotgrid = 61
         x = np.linspace(0, 1, num=plotgrid)
         y = np.linspace(0, 1, num=plotgrid)
         X, Y = np.meshgrid(x, y)
-        
         zs = np.array([self.predict([x,y]) for x,y in zip(np.ravel(X), np.ravel(Y))])
         Z = zs.reshape(X.shape)
         
-        spx = (self.X[:,0] * (self.normRange[0][1] - self.normRange[0][0])) + self.normRange[0][0]
-        spy = (self.X[:,1] * (self.normRange[1][1] - self.normRange[1][0])) + self.normRange[1][0] 
-        
-        return X,Y,Z,spx,spy
-
-def kriging_plot(i,k):
-    X,Y,Z,spx,spy=k.kdata()
-    plt.figure()
-    plt.contour(X,Y,Z,np.arange(-3.3, 3.5, .25).tolist(), cmap='jet')
-    plt.scatter(spx[11+i],spy[11+i],c='black',edgecolors='white',s=75,zorder=1000)
-    plt.xlim(0,1)
-    plt.ylim(0,1)
-    plt.savefig('iteration'+str(i)+'.jpg')
+        plt.figure()
+        plt.contour(X,Y,Z,np.arange(-3.3, 3.5, .25).tolist(), cmap='jet')
+        solution = [np.where(Z==np.min(Z))[0][0],np.where(Z==np.min(Z))[1][0]]
+        plt.scatter(X[solution[0],solution[1]],Y[solution[0],solution[1]],
+                    c='black',edgecolors='white',s=75,zorder=1000)
+        plt.xlim(0,1)
+        plt.ylim(0,1)
+        plt.savefig('iteration'+str(i)+'.jpg')
 
 def testfun(f,X):
     try:
@@ -61,14 +55,14 @@ for i in range(numiter/2):
     for point in newpoints:
         k.addPoint(point, testfun(function.venkataraman,point))
     k.train()
-    kriging_plot(2*i,k)
+    k.kplot(2*i)
         
     print 'Infill iteration {0} of {1}....'.format(2*i+1 + 1, numiter)
     newpoints = k.infill(1, method='ei')
     for point in newpoints:
         k.addPoint(point, testfun(function.venkataraman,point))
     k.train()
-    kriging_plot(2*i+1,k)
+    k.kplot(2*i+1)
     
 os.system('convert -delay 20 -loop 0 *.jpg images/kriging.gif')
 os.system('rm *.jpg')
